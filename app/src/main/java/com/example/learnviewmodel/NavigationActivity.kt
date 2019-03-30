275e6d277fe528037977a163e6d9f037e9a07283
ty61271 <ty61271@yahoo.com.tw>
2019-03-30 23:04:59 +0800

    ch2-4

---
+++ app/src/main/java/com/example/learnviewmodel/NavigationActivity.kt
@@ -0,0 +1,30 @@
+package com.example.learnviewmodel
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_navigation.*
+
+class NavigationActivity : AppCompatActivity() {
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_tasks -> {
+                message.setText(R.string.title_task)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_notes -> {
+                message.setText(R.string.title_note)
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_navigation)
+
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+}
